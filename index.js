846daa493dc57e2885df19bed3635ab16db24f61
ShahriarAnuvab <shahriarrahmananuvab@gmail.com>
2023-10-27 19:02:35 +0600

    setup the express

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const data = require("./data.json")
+const data = require("./data.json");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
@@ -95,31 +95,27 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/cart', async (req, res)=>{
-      const cursor = cartCollection.find()
-      const result = await cursor.toArray()
-      res.send(result)
-    })
+    app.get("/cart", async (req, res) => {
+      const cursor = cartCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    
-    app.get('/brand', async (req, res)=>{
-      res.send(data)
-    })
-    app.get('/brand/:Brand', async(req, res)=>{
+    app.get("/brand", async (req, res) => {
+      res.send(data);
+    });
+    app.get("/brand/:Brand", async (req, res) => {
       const brand = req.params.Brand.toLocaleLowerCase();
-     const result = data.filter(car=> car.brand.toLocaleLowerCase() === brand) || {}
-     res.send(result)
-    })
-
-
+      const result =
+        data.filter((car) => car.brand.toLocaleLowerCase() === brand) || {};
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
-
-    
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
